7e6a13cc2409cbcae1c66cacbcf766708f405821
Jakon <jakob.geusen@icloud.com>
2020-11-26 13:58:46 +0100

    testing fixed input feature

--- fitting/Fitter.py
+++ fitting/Fitter.py
@@ -15,7 +15,6 @@ def create_fitter_from_configuration(fit_model_configuration):
                     data_type=fit_model_configuration.data_type,
                     save_losses=fit_model_configuration.save_losses,
                     constant_fixed_input=fit_model_configuration.constant_input)
-    print(fit_model_configuration.constant_input)
     return fitter
 
 
