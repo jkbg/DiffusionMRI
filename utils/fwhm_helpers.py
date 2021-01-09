5380f33e3772190e4bc38e0a494a77c4467b21a8
Jakon <jakob.geusen@icloud.com>
2021-01-09 13:28:47 +0100

    Critical Alpha values introduced in fwhm calculations

--- utils/fwhm_helpers.py
+++ utils/fwhm_helpers.py
@@ -6,7 +6,7 @@ def logistic_function(x, alpha, beta, gamma, c):
     return alpha / (1. + np.exp((x - beta) / gamma)) + c
 
 
-def calculate_fwhm(image, accuracy_factor=100, max_iterations=1000):
+def calculate_fwhm(image, accuracy_factor=100, max_iterations=1000, critical_alpha=1e-02):
     np.seterr(all='ignore')
     number_of_pixels = image.shape[1]
     row_fwhms = []
@@ -45,11 +45,14 @@ def calculate_fwhm(image, accuracy_factor=100, max_iterations=1000):
         half_max = np.max(differences) / 2.
         indices = np.where(np.diff(np.sign(differences - half_max)))[0]
         row_fwhm = (indices[-1] - indices[0]) / accuracy_factor
-
         fitted_parameters.append([a, b, g, c])
         row_fwhms.append(row_fwhm)
 
-    return np.mean(row_fwhms), np.mean(np.array(fitted_parameters), axis=0)
+    avg_parameters = np.mean(np.array(fitted_parameters), axis=0)
+    if avg_parameters[0] < critical_alpha:
+        return np.nan
+
+    return np.mean(row_fwhms)
 
 
 def calculate_vertical_profile(image):
