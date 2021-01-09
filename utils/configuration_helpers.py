d4de8298faf09036295677ba89e0b3182b9d4804
Jakon <jakob.geusen@icloud.com>
2021-01-09 23:12:36 +0100

    Updated Configuration helpers

--- utils/configuration_helpers.py
+++ utils/configuration_helpers.py
@@ -4,6 +4,35 @@ from itertools import product
 from time import gmtime, strftime
 
 
+class ModelFittingConfiguration:
+    def __init__(self):
+        self.image_dimensions = [100, 100, 1]
+        self.result_path = 'data/results'
+
+        self.model_type = 'deep'
+        self.input_shape = [14, 14]
+        self.number_of_layers = 5
+        self.number_of_hidden_channels = 40
+
+        self.number_of_iterations = 3000
+        self.number_of_runs = 10
+        self.learning_rate = 0.1
+        self.convergence_check_length = None
+        self.log_frequency = 100
+        self.find_best = False
+        self.save_losses = False
+        self.constant_input = False
+
+        self.data_type = torch.cuda.FloatTensor
+
+    def __str__(self):
+        dictionary = self.__dict__
+        result = ""
+        for key in dictionary:
+            result += key + ": " + str(dictionary[key]) + "  " + os.linesep
+        return result
+
+
 class GridsearchConfiguration:
     def __init__(self, ):
         self.image_dimensions = [100, 100, 1]
@@ -42,7 +71,3 @@ class GridsearchConfiguration:
         for key in dictionary:
             result += key + ": " + str(dictionary[key]) + "  " + os.linesep
         return result
-
-
-if __name__ == "__main__":
-    print(get_gridsearch_configuration())
