39d57a24cb17b456c3b83baef286be44641d63a8
Jakon <jakob.geusen@icloud.com>
2021-01-11 11:08:48 +0100

    Exception raised if model_type not 'deep' or 'conv'

--- models/model_creation.py
+++ models/model_creation.py
@@ -13,18 +13,6 @@ def create_model_from_configuration(fit_model_configuration):
                             image_dimensions=fit_model_configuration.image_dimensions,
                             number_of_layers=fit_model_configuration.number_of_layers,
                             number_of_hidden_channels=fit_model_configuration.number_of_hidden_channels)
-    return model
-
-
-def create_model_from_parameter_combination(parameter_combination, image_dimensions):
-    if parameter_combination[0] == "conv":
-        model = ConvolutionDecoder(input_shape=parameter_combination[1],
-                                   image_dimensions=image_dimensions,
-                                   number_of_layers=parameter_combination[2],
-                                   number_of_hidden_channels=parameter_combination[3])
-    elif parameter_combination[0] == "deep":
-        model = DeepDecoder(input_shape=parameter_combination[1],
-                            image_dimensions=image_dimensions,
-                            number_of_layers=parameter_combination[2],
-                            number_of_hidden_channels=parameter_combination[3])
+    else:
+        raise Exception(fit_model_configuration.model_type + 'is not a known model type.')
     return model
--- utils/configuration_helpers.py
+++ utils/configuration_helpers.py
@@ -34,7 +34,7 @@ class ModelFittingConfiguration:
 
 
 class GridsearchConfiguration:
-    def __init__(self, ):
+    def __init__(self):
         self.number_of_images = 20
         self.image_dimensions = [100, 100, 1]
         self.image_path = 'data/imagenet_v2_top_images'
@@ -57,6 +57,26 @@ class GridsearchConfiguration:
         self.data_type = torch.cuda.FloatTensor
 
     def generate_parameter_combinations(self):
+        fit_config = ModelFittingConfiguration()
+        fit_config.image_dimensions = self.image_dimensions
+        fit_config.result_path = 'data/results'
+
+        fit_config.model_type = 'deep'
+        fit_config.input_shape = [14, 14]
+        fit_config.number_of_layers = 5
+        fit_config.number_of_hidden_channels = 40
+
+        fit_config.number_of_iterations = 3000
+        fit_config.number_of_runs = 10
+        fit_config.learning_rate = 0.1
+        fit_config.convergence_check_length = None
+        fit_config.log_frequency = 100
+        fit_config.find_best = False
+        fit_config.save_losses = False
+        fit_config.constant_input = False
+
+        fit_config.data_type = torch.cuda.FloatTensor
+
         model_types = self.model_types
         input_shapes = self.input_shapes
         numbers_of_layers = self.numbers_of_layers
@@ -74,7 +94,9 @@ class GridsearchConfiguration:
             path += '/' + str(parameter_combination[3])
             path += '/'
             paths.append(path)
-        return parameter_combinations, paths
+
+        self.parameter_combinations = parameter_combinations
+        self.result_paths = paths
 
     def __str__(self):
         dictionary = self.__dict__
